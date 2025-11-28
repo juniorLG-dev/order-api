a00e4cf392ea515c8d6c28913ec7619d7725d427
juniorLG-dev <contaproxadrez2023@gmail.com>
2025-11-28 20:04:18 -0300

    factory for event setup implemented

---
+++ common/infra/factory/register_events.go
@@ -0,0 +1,20 @@
+package factory
+
+import (
+	"errors"
+	"order/common/event"
+)
+
+var registerEvents = make(map[string]func() event.Event)
+
+func RegisterEvent(id string, event func() event.Event) error {
+	if _, ok := registerEvents[id]; ok {
+		return errors.New("this event has already been registered")
+	}
+	registerEvents[id] = event
+	return nil
+}
+
+func GetEvent(id string) func() event.Event {
+	return registerEvents[id]
+}
